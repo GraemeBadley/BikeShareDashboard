b9b1178a8f996f21576753e8a1608b2889553aad
GraemeBadley <gbadley1@gmail.com>
2026-03-07 19:22:23 -0500

    added timestamps to python records

--- ingestion/bike_share_client.py
+++ ingestion/bike_share_client.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timezone
 
 API_BASE = "https://tor.publicbikesystem.net/ube/gbfs/v1/en/"
 STATION_STATUS_ENDPOINT = "station_status"
@@ -10,14 +11,14 @@ def fetch_station_status():
     
     data = _get_api_call(url)
 
-    print(data["data"]["stations"][0])
+    return data["data"]["stations"]
 
 def fetch_station_information():
     url = API_BASE + STATION_INFORMATION_ENDPOINT
 
     data = _get_api_call(url)
 
-    print(data["data"]["stations"][0])
+    return data["data"]["stations"]
 
 def _get_api_call(url):
     
@@ -29,3 +30,38 @@ def _get_api_call(url):
     else:
         print(f"Request failed with status code {response.status_code}")
         print(response.text)
+        raise Exception("ApiRequestError")
+    
+def combine_station_data(station_status,station_information):
+    full_stations_info = []
+
+    # Pulling out id for station status to be searchable
+    station_status_map = {station['station_id']: station for station in station_status}
+
+    for station in station_information:
+
+        
+        id = station['station_id']
+
+        
+
+        timestamp = datetime.now(timezone.utc).isoformat()
+        if id in station_status_map.keys():
+            full_station_info = {**station,"station_status":station_status_map[id]}
+            full_station_info["timestamp"] = timestamp
+
+            full_stations_info.append(full_station_info)
+        else:
+            full_station_info = {**station,"station_status":None}
+            full_station_info["timestamp"] = timestamp
+
+            full_stations_info.append(full_station_info)
+
+        
+        
+
+        
+
+    return full_stations_info
+        
+
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from ingestion.bike_share_client import fetch_station_information,fetch_station_status
+from ingestion.bike_share_client import fetch_station_information,fetch_station_status,combine_station_data
 
 def main():
     print("Bike station status")
-    fetch_station_status()
+    station_status = fetch_station_status()
+    #print(station_status[0])
     print("Bike station information")
-    fetch_station_information()
+    station_info = fetch_station_information()
+    #print(station_info[0])
+
+    full_info_stations = combine_station_data(station_status,station_info)
+
+    print(full_info_stations[0])
 
 
 if __name__ == "__main__":
